075c316b6951847e6165061e7e9e56a588980567
raazijawad <mohdraazi105@gmail.com>
2025-06-15 02:55:39 -0700

    added customized feature box to the bottom

--- src/components/FeaturesSection.jsx
+++ src/components/FeaturesSection.jsx
@@ -19,25 +19,31 @@ const features = [
 ]
 
 const FeaturesSection = () => {
-  return (
-    <section className='max-w-7xl mx-auto px-4 py-16'>
-        {/* heading texts */}
-        <div className='text-center mb-12'>
-            <h2 className='text-3xl font-bold mb-4'>How can we help your buisness?</h2>
-            <p className='text-gray-600'>when you resell besnik, you build trust and increase</p>
-        </div>
+    return (
+        <section className='max-w-7xl mx-auto px-4 py-16'>
+            {/* heading texts */}
+            <div className='text-center mb-12'>
+                <h2 className='text-3xl font-bold mb-4'>How can we help your buisness?</h2>
+                <p className='text-gray-600'>when you resell besnik, you build trust and increase</p>
+            </div>
 
-        {/* features box */}
-        <div>
-            {features.map((features, index) => (
-                <div>
-                    <div>{features.icon}</div>
-                </div>
-            ))}
+            {/* features box */}
+            <div className='grid grid-cols-1 md:grid-cols-3 gap-8'>
+                {features.map((features, index) => (
+                    <div key={index} className='flex flex-col text-center items-center p-6'>
+                        <div className='w-24 h-24 rounded-full mb-6 flex items-center justify-center' style={{
+                            backgroundColor: index === 0 ? "#F1EFFD" : index === 1 ? "#FEE7E7" : "#FFF3E4"
+                        }}>
+                            <div className='text-3xl'>{features.icon}</div>
+                        </div>
+                        <h3 className='text-2xl font-medium mb-3'>{features.title}</h3>
+                        <p className='text-gray-500'>{features.Description}</p>
+                    </div>
+                ))}
 
-        </div>
-    </section>
-  )
+            </div>
+        </section>
+    )
 }
 
 {/* …or push an existing repository from the command line
