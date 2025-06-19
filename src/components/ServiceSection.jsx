cff0af4c2e1884efb268dd5803cb9e8b7b0b2be6
raazijawad <mohdraazi105@gmail.com>
2025-06-19 00:41:47 -0700

    added service section

---
+++ src/components/ServiceSection.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const ServiceSection = () => {
+  return (
+    <section className='py-20 container mx-auto px-4 sm:px-4 lg:px-8'>
+        <div className=''>
+            {/* Header */}
+            <div className=''>
+                
+            </div>
+            {/* Service cards */}
+            <div>
+
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default ServiceSection
