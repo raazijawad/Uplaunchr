5874b6af5d88e116dde50d11f32ce3040b034d78
raazijawad <mohdraazi105@gmail.com>
2025-06-20 02:24:17 -0700

    added heading and description for the testimonial section

--- src/components/TestimonialSection.jsx
+++ src/components/TestimonialSection.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 const TestimonialSection = () => {
   return (
-    <div>
-      
-    </div>
+     <section className='py-16 px-4 max-w-7xl mx-auto'>
+        <div className='text-center'>
+            <h2 className='text-3xl font-bold mb-4'>What our happy client say</h2>
+            <p className='text-gray-600 text-sm font-medium'>Things that make it the best place to start trading</p>
+        </div>
+     </section>
   )
 }
 
