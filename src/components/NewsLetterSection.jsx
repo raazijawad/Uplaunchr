1e6b01d8162f97fff6a05944150a3e583aed758a
raazijawad <mohdraazi105@gmail.com>
2025-06-23 05:44:06 -0700

    added right and left content

--- src/components/NewsLetterSection.jsx
+++ src/components/NewsLetterSection.jsx
@@ -10,11 +10,19 @@ const NewsLetterSection = () => {
                                 hidden md:block'>
                 </div>
 
-                <div>
+                <div className='relative flex flex-col lg:flex-row items-center justify-between gap-8 lg:gap-12'>
                     {/* left content */}
                     <div className='text-white max-w-lg text-center md:text-left'>
                         <h2 className='text-2xl sm:text-3xl lg:text-4xl font-medium mb-4'>Subscribe Newsletter</h2>
                         <p className='text-blue-100 text-sm sm:text-base'>best cook and best delivery guys all at your service. Hot tasty food</p>
+                    </div>
+
+                    {/* Right content */}
+                    <div>
+                        <input type="email" placeholder='Enter Your Email'
+                               className='w-full bg-white sm:w-auto md:w-80 px-4 sm:px-4 py-3 sm:py-4 rounded-xl
+                                          sm:rounded-l-xl sm:rounded-r-none focus:outline-none' />
+                        
                     </div>
                 </div>
 
