2732c3279fd85fee5717f96fd5a99f8431d7826f
raazijawad <mohdraazi105@gmail.com>
2025-06-24 06:18:27 -0700

    paragraph and Icon added

--- src/components/FooterSection.jsx
+++ src/components/FooterSection.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FaFacebookF } from 'react-icons/fa'
 
 const FooterSection = () => {
     return (
@@ -7,13 +8,23 @@ const FooterSection = () => {
                 <div>
                     {/* Brand column */}
                     <div>
-                        <div className="flex items-center gap-1 cursor-pointer animate-pulse">
-                            <div className="text-sm font-medium text-blue-900">
-                                Up <span></span>Launcher
+                        <div className='flex gap-2 items-center'>
+                            <div className="flex items-center gap-1 cursor-pointer animate-pulse">
+                                <div className="text-sm font-medium text-blue-900">
+                                    Up <span></span>Launcher
+                                </div>
                             </div>
-                            <span>The Next Design</span>
+                            <span className='text-xl font-medium ml-1'>The Next Design</span>
+                        </div>
+                        <p className='text-gray-600 mb-6'>The copy warned the little blind text, that where it came from it would 
+                           have been rewritten a thousand times
+                        </p>
+                          
+                        <div>
+                            <a href="#w"><FaFacebookF/></a>
                         </div>
                     </div>
+
                 </div>
             </div>
         </footer>
