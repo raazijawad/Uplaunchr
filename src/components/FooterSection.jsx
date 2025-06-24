a526fba73dc3d356ead74df63bbc514eae1dd448
raazijawad <mohdraazi105@gmail.com>
2025-06-24 06:43:04 -0700

    Left side " The Next Design" part added (1/4 )

--- src/components/FooterSection.jsx
+++ src/components/FooterSection.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import { FaFacebookF } from 'react-icons/fa'
+import { FaFacebookF, FaLinkedin, FaLinkedinIn, FaTwitter } from 'react-icons/fa'
 
 const FooterSection = () => {
     return (
-        <footer className='bg-gray-500'>
+        <footer className='bg-gray-50'>
             <div className='container mx-auto px-4 sm:px-6 lg:px-8  py-16'>
-                <div>
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-12 gap-8 lg:gap-12'>
                     {/* Brand column */}
-                    <div>
-                        <div className='flex gap-2 items-center'>
+                    <div className='lg:col-span-4'>
+                        <div className='flex gap-2 items-center mb-6'>
                             <div className="flex items-center gap-1 cursor-pointer animate-pulse">
                                 <div className="text-sm font-medium text-blue-900">
                                     Up <span></span>Launcher
@@ -20,8 +20,24 @@ const FooterSection = () => {
                            have been rewritten a thousand times
                         </p>
                           
-                        <div>
-                            <a href="#w"><FaFacebookF/></a>
+                        <div className='flex gap-8'>
+                            <a href="#" className='w-10 h-10 bg-gray-200 rounded-full flex items-center
+                                                  justify-center text-gray-600 hover:bg-blue-600 hover:text-white
+                                                  transition-colors duration-300'>
+                                <FaFacebookF className='size-5'/>
+                            </a>
+
+                            <a href="#" className='w-10 h-10 bg-gray-200 rounded-full flex items-center
+                                                  justify-center text-gray-600 hover:bg-blue-600 hover:text-white
+                                                  transition-colors duration-300'>
+                                <FaTwitter className='size-5'/>
+                            </a>
+
+                            <a href="#" className='w-10 h-10 bg-gray-200 rounded-full flex items-center
+                                                  justify-center text-gray-600 hover:bg-blue-600 hover:text-white
+                                                  transition-colors duration-300'>
+                                <FaLinkedin className='size-5'/>
+                            </a>
                         </div>
                     </div>
 
