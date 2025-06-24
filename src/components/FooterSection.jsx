f5b3a68d64cae82a0397a43807baa1b4b1917c94
raazijawad <mohdraazi105@gmail.com>
2025-06-24 06:08:51 -0700

    Brand column added

--- src/components/FooterSection.jsx
+++ src/components/FooterSection.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 
 const FooterSection = () => {
-  return (
-    <section>
-        
-    </section>
-  )
+    return (
+        <footer className='bg-gray-500'>
+            <div className='container mx-auto px-4 sm:px-6 lg:px-8  py-16'>
+                <div>
+                    {/* Brand column */}
+                    <div>
+                        <div className="flex items-center gap-1 cursor-pointer animate-pulse">
+                            <div className="text-sm font-medium text-blue-900">
+                                Up <span></span>Launcher
+                            </div>
+                            <span>The Next Design</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
 }
 
 export default FooterSection
