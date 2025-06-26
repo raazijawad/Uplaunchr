af313bbc36474cff9e6868945af149586652d95e
raazijawad <mohdraazi105@gmail.com>
2025-06-26 11:51:59 -0700

    added framer motion

--- src/components/MoniterSection.jsx
+++ src/components/MoniterSection.jsx
@@ -4,6 +4,7 @@ import { IoIosArrowRoundForward } from "react-icons/io";
 import MRockter2 from "../assets/MoniterRocket.webp"
 import { motion } from 'framer-motion';
 import { fadeIn } from '../ultils/motion';
+import { LuMonitorPause } from 'react-icons/lu';
 
 const MoniterSection = () => {
     return (
@@ -22,19 +23,25 @@ const MoniterSection = () => {
                     <motion.p 
                     variants={fadeIn('up', 0.4)}
                     className='text-green-500 font-semibold'>MONITER</motion.p>
-                    <h2 className='text-3xl md:text-4xl font-bold text-neutral-900 mt-4 mb-6' >Indroducing best mobile <br />
+                    <motion.h2 
+                    variants={textVariant(0.5)}
+                    className='text-3xl md:text-4xl font-bold text-neutral-900 mt-4 mb-6' >Indroducing best mobile <br />
                         carousels
-                    </h2>
-                    <p className='text-gray-600 mb-6'>
+                    </motion.h2>
+                    <motion.p 
+                    variants={fadeIn('up', 0.6)}
+                    className='text-gray-600 mb-6'>
                         Before the ship is really back. Round, round, all around the world. Round,
                         all around the world. Round, all around the world. Round, all around the world.
-                    </p>
+                    </motion.p>
 
-                    <a href="#" className='flex text-blue-500 font-semibold items-center gap-2 
+                    <motion.a 
+                    variants={fadeIn('up', 0.7)}
+                    href="#" className='flex text-blue-500 font-semibold items-center gap-2 
                                                            hover:gap-4 transition-all hover:text-blue-600'>
                         Learn more about monitering
                         <IoIosArrowRoundForward className='size-8' />
-                    </a>
+                    </motion.a>
                 </motion.div>
                 
                 {/* Right */}
