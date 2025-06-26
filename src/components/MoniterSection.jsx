4fd9bdf213d885d63516b2345e4a32b27ce23480
raazijawad <mohdraazi105@gmail.com>
2025-06-26 11:49:15 -0700

    added framer motion

--- src/components/MoniterSection.jsx
+++ src/components/MoniterSection.jsx
@@ -2,15 +2,26 @@ import React from 'react'
 import MRockter from "../assets/rocketfly.jpg"
 import { IoIosArrowRoundForward } from "react-icons/io";
 import MRockter2 from "../assets/MoniterRocket.webp"
+import { motion } from 'framer-motion';
+import { fadeIn } from '../ultils/motion';
 
 const MoniterSection = () => {
     return (
-        <section className='max-w-7xl mx-auto px-4 py-16 md:py-24'>
+        <motion.section
+         variants={fadeIn('up', 0.2)}
+         initial="hidden"
+         whileInView="show"
+         viewport={{ once: true }}
+        className='max-w-7xl mx-auto px-4 py-16 md:py-24'>
             <div className='flex flex-col md:flex-row items-center justify-between gap-12 md:gap-24'>
                 {/* Left */}
 
-                <div className='md:w-1/2 w-full'>
-                    <p className='text-green-500 font-semibold'>MONITER</p>
+                <motion.div
+                variants={fadeIn('right', 0.3)}
+                className='md:w-1/2 w-full'>
+                    <motion.p 
+                    variants={fadeIn('up', 0.4)}
+                    className='text-green-500 font-semibold'>MONITER</motion.p>
                     <h2 className='text-3xl md:text-4xl font-bold text-neutral-900 mt-4 mb-6' >Indroducing best mobile <br />
                         carousels
                     </h2>
@@ -24,7 +35,7 @@ const MoniterSection = () => {
                         Learn more about monitering
                         <IoIosArrowRoundForward className='size-8' />
                     </a>
-                </div>
+                </motion.div>
                 
                 {/* Right */}
                 <div className='md:w-1/2 w-full'>
@@ -34,7 +45,7 @@ const MoniterSection = () => {
                 
             </div>
 
-        </section>
+        </motion.section>
     )
 }
 
