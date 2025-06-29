65f8690ef46bf01300efddef82ac8276dbc07aa2
raazijawad <mohdraazi105@gmail.com>
2025-06-29 03:47:45 -0700

    added moiton frame

--- src/components/ServiceSection.jsx
+++ src/components/ServiceSection.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
+import { motion } from 'framer-motion'
+import { BiTime } from 'react-icons/bi'
 import { BsStack } from "react-icons/bs"
+import { FiSettings } from 'react-icons/fi'
+import { HiLightBulb } from 'react-icons/hi'
+import { fadeIn, textVariant } from '../ultils/motion'
 {/*import { HiLightBulb } from "react-icone/hi"*/}
 const services = [
     {
@@ -9,20 +14,20 @@ const services = [
         link: "#learn-more"
     },
     {
-        icone: <BsStack className='w-8 h-8 text-indigo-600'/>,
-        title: "Web Design",
+        icone: <HiLightBulb  className='w-8 h-8 text-indigo-600'/>,
+        title: "Ad-Creatives", 
         description: "One for all and all for one, Muskehounds are always ready.",
         link: "#learn-more"
     },
     {
-        icone: <BsStack className='w-8 h-8 text-indigo-600'/>,
-        title: "Web Design",
+        icone: <FiSettings className='w-8 h-8 text-indigo-600'/>,
+        title: "Automation",
         description: "One for all and all for one, Muskehounds are always ready.",
         link: "#learn-more"
     },
     {
-        icone: <BsStack className='w-8 h-8 text-indigo-600'/>,
-        title: "Web Design",
+        icone: <BiTime className='w-8 h-8 text-indigo-600'/>,
+        title: "Infographics",
         description: "One for all and all for one, Muskehounds are always ready.",
         link: "#learn-more"
     }
@@ -31,16 +36,26 @@ const services = [
 const ServiceSection = () => {
     return (
         <section className='py-20 container mx-auto px-4 sm:px-4 lg:px-8' id='services'>
-            <div className='grid grid-cols-1 md:grid-cols-2'>
+            <motion.div
+            variants={fadeIn('up', 0.3)}
+            className='grid grid-cols-1 md:grid-cols-2'>
                 {/* Header */}
-                <div className='md:mt-12'>
-                    <h2 className=' text-3xl md:text-4xl mb-6 font-bold md:w-4/5'>
+                <motion.div 
+                variants={fadeIn('right', 0.4)}
+                className='md:mt-12'>
+                    <motion.h2
+                    variants={textVariant(0.2)}
+                    className=' text-3xl md:text-4xl mb-6 font-bold md:w-4/5'>
                         Future of supports with new shapes
-                    </h2>
-                    <p className='text-gray-600 text-lg mb-4 md:w-4/5'>
+                    </motion.h2>
+                    <motion.p 
+                    variants={fadeIn('up', 0.5)}
+                    className='text-gray-600 text-lg mb-4 md:w-4/5'>
                         Discuss your goal, determine success metrics, identify problems
-                    </p>
-                    <div className='space-y-3'>
+                    </motion.p>
+                    <motion.div
+                    variants={fadeIn('up', 0.6)}
+                    className='space-y-3'>
                         <div className='flex gap-2 items-center '>
                             <div className='w-5 h-5 rounded-full bg-indigo-100 flex items-center justify-center' >
                                 <div className='w-2.5 h-2.5 rounded-full bg-indigo-600'></div>
@@ -56,13 +71,13 @@ const ServiceSection = () => {
                             </div>
                             <span className='text-gray-600'>Development bring </span>
                         </div>
-                    </div>
+                    </motion.div>
 
                     <button className='px-8 py-3 bg-blue-600 text-white rounded-full cursor-pointer
                                      hover:bg-blue-700 transition-colors'>
                         Get started
                     </button>
-                </div>
+                </motion.div>
                 {/* Service cards */}
                 <div className='mt-8 grid grid-cols-1 md:grid-cols-2 gap-8'>
                     {
@@ -82,7 +97,7 @@ const ServiceSection = () => {
                         ))
                     }
                 </div>
-            </div>
+            </motion.div>
         </section>
     )
 }
